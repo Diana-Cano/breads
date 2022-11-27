9acbc3fcf30a1f12ad06d43f0473501f41de2f68
Diana-Cano <dianacanoo@icloud.com>
2022-11-27 14:07:23 -0800

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -13,10 +13,19 @@ breads.get('/', (req, res) => {
 })
 
 
-module.exports = breads
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
+
+
+module.exports = breads
   
